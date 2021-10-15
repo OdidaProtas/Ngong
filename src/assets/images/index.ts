9f99dedb5d6bb03296a0660182871c746e757a2a
OdidaProtas <bryodiiidah@gmail.com>
2021-10-15 09:20:25 +0300

    refactor project

--- src/assets/images/index.ts
+++
@@ -1,6 +0,0 @@
-import AiIcon from "./ai.svg";
-import CustomerIcon from "./customer.svg";
-import InsightIcon from "./insight.svg";
-import BusinessIcon from "./business.svg";
-
-export { AiIcon, CustomerIcon, InsightIcon, BusinessIcon };

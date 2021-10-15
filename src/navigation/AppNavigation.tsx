5780055bf7b153946d1229ecf9f300521e9ab84e
OdidaProtas <bryodiiidah@gmail.com>
2021-10-15 17:18:42 +0300

    preview page

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,5 +1,26 @@
+import { Button, Typography } from "@mui/material";
 import React from "react";
 
+import Logo from "../assets/images/logo.png";
+
 export default function AppNavigation() {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        <img height={300} src={Logo} alt="Protus Logo" />
+        <Typography variant="h4" color="secondary">
+          Coming soon...
+        </Typography>
+        {/* <Button sx={{ marginTop: "18px", textTransform: "none" }}>
+          Early access
+        </Button> */}
+      </div>
+    </div>
+  );
 }
